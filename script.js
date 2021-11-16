d1b21fb60deacd9bed58c3241a15177e26c0068a
Chris Olry <32845605+colry@users.noreply.github.com>
2021-11-16 08:40:04 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,26 +2,28 @@ function compute()
 {
 	// From old code;
     // p = document.getElementById("principal").value;
-	
 	// Get the principal valuce.
-    var principal = document.getElementById("principal").value;
+	var principal = document.getElementById("principal").value;
 	
-	// Get the rate value.
-	var rate = document.getElementById("rate").value
-	
-	// Get the years value.
-	var years = document.getElementById("years").value;
-	
-	// Calculate inteterest:
-	var interest = principal * years * rate /100;
-	// Logic to convert the 'No of Years' into the actual year in the future.
-	var year = new Date().getFullYear()+parseInt(years);
-	
-	// Patch for output.
-	var amount = interest
-	
-	// Output
-	document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+	if (principal <= 0) {
+		document.getElementById("result").innerHTML="Please enter a principal amount greater than 0."} else {
+			
+			// Get the rate value.
+			var rate = document.getElementById("rate").value
+			
+			// Get the years value.
+			var years = document.getElementById("years").value;
+			
+			// Calculate inteterest:
+			var interest = principal * years * rate /100;
+			// Logic to convert the 'No of Years' into the actual year in the future.
+			var year = new Date().getFullYear()+parseInt(years);
+			
+			// Patch for output.
+			var amount = interest
+			
+			// Output
+			document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"};
 }
 
 function updateRate() 
